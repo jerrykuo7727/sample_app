af7dac997771887dcec41947ce6ea2676aeb34c5
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-08-28 15:09:45 +0800

    Add test for User.name length validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,4 +19,9 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "     "
     assert_not @user.valid?
   end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
 end
