a60772706da4a0985472ed585c31c7ee6989977f
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-02 12:31:54 +0800

    Show flash message after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "帳號成功註冊！"
       redirect_to @user
     else
       render 'new'
