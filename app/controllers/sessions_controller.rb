3e6d30af224668535722a45dec9a8b6ebc771f91
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-03 12:49:27 +0800

    Add test for user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash[:danger] = '錯誤的電子信箱或密碼' # Not quite right
+      render 'new'
+    end
   end
 
   def destroy
