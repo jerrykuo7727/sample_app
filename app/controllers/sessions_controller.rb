4e4da3c2ac7907d0b9d45289efc3a325fc70ab52
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-03 13:03:39 +0800

    Add session to store user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = '錯誤的電子信箱或密碼'
       render 'new'
