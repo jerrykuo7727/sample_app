183746f3eb1bc3d7a5ccc94e7390f6ff1bf4dc60
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-03 13:32:47 +0800

    Log in the user upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "帳號註冊成功！"
       redirect_to @user
     else
