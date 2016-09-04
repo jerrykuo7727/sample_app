6e5a302788f6da26cb04867e884f9e47abe2642c
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-04 09:17:38 +0800

    Update current_user to return corresponding to cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,9 +11,17 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
 
-  # Returns the current logged-in user (if any).
+  # Returns the current corresponding to the remember token cookie.
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif cookies.signed[:user_id]
+      user = User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Returns true if the user is logged in, false otherwise.
