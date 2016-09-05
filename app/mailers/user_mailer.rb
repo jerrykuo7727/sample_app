4c06bec813d9963ba2764a5a8359ac1f61f02760
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 17:26:20 +0800

    Write UserMail#password_reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "樣品App帳號驗證信"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "樣品App密碼重置信"
   end
 end
