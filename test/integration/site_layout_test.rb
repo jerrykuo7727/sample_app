85858f51ff55035c7b6f0177d52806de160cdc4b
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 10:56:08 +0800

    Update integration test of site layout

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,14 +1,38 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  
-  test 'layout links' do 
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'layout links of unlogged-in users' do 
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
+    get contact_path
+    assert_select "title", full_title("聯絡方式")
+    get signup_path
+    assert_select "title", full_title("註冊帳號")
+  end
+
+  test 'layout links of logged-in users' do 
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user_path(@user)
     get contact_path
     assert_select "title", full_title("聯絡方式")
     get signup_path
