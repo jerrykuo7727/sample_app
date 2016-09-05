7805a9f89b8d20df01e510290d2b6bfcf3045a4b
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 17:20:52 +0800

    Update flash message of password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,7 @@ class PasswordResetsController < ApplicationController
       flash[:info] = "密碼重置信已發送至電子信箱"
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = "此電子信箱尚未註冊"
       render 'new'
     end
   end
