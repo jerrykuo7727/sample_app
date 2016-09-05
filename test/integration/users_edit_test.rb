a56d567a52702864914416dba80e568651713006
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 10:23:02 +0800

    Add tests for trying to edit as wrong user

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "unsuccessful edit" do
@@ -45,5 +46,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
