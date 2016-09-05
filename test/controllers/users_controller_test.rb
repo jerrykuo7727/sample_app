8fa8677717ca3717becb92a3ccd8e5d93ad06a74
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 10:38:49 +0800

    Testing the index action redirect

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
   
   test "should get new" do
     get signup_path
