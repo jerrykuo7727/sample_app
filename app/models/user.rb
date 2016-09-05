5e008529c0d8cf688cd7ccd8aed94e75a2a149f7
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 15:44:34 +0800

    Use modified authenticated? method in every files.

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,10 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user.
