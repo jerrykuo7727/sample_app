a47b9e09b752040494e8bf9c7488d7a519b2aa7e
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 10:36:38 +0800

    Update test of friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -78,5 +78,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
+    assert_nil session[:forwarding_url]
   end
 end
