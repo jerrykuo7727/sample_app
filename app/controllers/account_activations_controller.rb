bf3f79940dc8356756eed46b1a0d9205163e8308
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 15:48:57 +0800

    Add AccountActivations#edit action

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "帳號驗證成功！"
+      redirect_to user
+    else
+      flash[:danger] = "無效的驗證連結"
+      redirect_to root_url
+    end
+  end
 end
