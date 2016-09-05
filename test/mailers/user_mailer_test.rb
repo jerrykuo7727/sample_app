c1a6325a33052c5b0b078db7fac85396ada2071c
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 15:32:26 +0800

    Add test of email implementation

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,20 +1,19 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+
   test "account_activation" do
-    mail = UserMailer.account_activation
-    assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
+    assert_equal "樣品App帳號驗證信", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name,               mail.html_part.body.raw_source
+    assert_match user.activation_token,   mail.html_part.body.raw_source
+    assert_match CGI.escape(user.email),  mail.html_part.body.raw_source
+    assert_match user.name,               mail.text_part.body.raw_source
+    assert_match user.activation_token,   mail.text_part.body.raw_source
+    assert_match CGI.escape(user.email),  mail.text_part.body.raw_source
   end
-
-  test "password_reset" do
-    mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
-
 end
