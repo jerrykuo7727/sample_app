db6ef7121248cd0004b73cecca863e0f0fcc017f
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 18:13:11 +0800

    Clear the reset digest on successful password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -28,6 +28,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "密碼重置成功！"
       redirect_to @user
     else
