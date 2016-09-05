7fabc9bbfba37de9213d27a5ffe274b6b8b1ca11
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-05 16:03:28 +0800

    Add account activation to the user signup test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message = "帳號尚未驗證，"
+        message += "請至電子信箱收取驗證信" 
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = '錯誤的電子信箱或密碼'
       render 'new'
